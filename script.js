adbd81725683cb2fe36677e1acde5fcc89858492
Vamshi Reddy <106398124+mv735065@users.noreply.github.com>
2024-11-04 19:07:05 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,14 @@
-//your JS code here. If required.
+let btn = document.querySelector("input");
+ let select = document.getElementById("colorSelect");
+btn.addEventListener('click', removeOption);
+
+function removeOption(event) {
+   
+    let value = select.value;
+    let options = document.querySelectorAll("option");
+    for (let opt of options) {
+        if (opt.innerText === value) {
+            opt.remove();
+        }
+    }
+}
